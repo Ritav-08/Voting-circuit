d91694506b2dae0c931d627e54120481c461fb34
Ritav <ritav08092004@gmail.com>
2026-04-21 18:28:03 +0530

    Add files via upload

---
+++ Design.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.04.2026 09:25:04
+// Design Name: 
+// Module Name: mVoting
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mVoting(
+   input [3:0]a_i, 
+   output y_o, //majourity result
+   output t_o //tie
+);
+
+//net(s)
+wire [2:0]count;
+
+//Comment: for commented design, make t_o of type reg.
+//assign y_o = (a_i[3] & a_i[2] & a_i[1]) | (a_i[3] & a_i[2] & a_i[0]) | (a_i[3] & a_i[0] & a_i[1]) | (a_i[2] & a_i[1] & a_i[0]) | (a_i[3] & a_i[2] & a_i[1] & a_i[0]);
+//always@(*) //in 4 bit input, number of 1s = number of 0s, y_o will be 0 as per above expression
+//if(y_o || a_i == 4'b0)
+//   t_o = 1'b0;
+//else 
+//   t_o = ~(^a_i); //1s are even, i.e. two
+
+assign count = a_i[3] + a_i[2] + a_i[1] + a_i[0];
+assign y_o = (count >= 4'd3);
+assign t_o = (count == 4'd2);
+
+endmodule
---
+++ Testbench.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.04.2026 09:25:34
+// Design Name: 
+// Module Name: tb_mVoting
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_mVoting();
+reg [3:0]a_ti; 
+wire y_to;
+wire t_to;
+
+//net(s)
+integer i;
+
+//instantiation
+mVoting UUT(.a_i(a_ti), 
+   .y_o(y_to), 
+   .t_o(t_to));
+
+//feeding
+initial begin
+a_ti = 4'b0000;
+for(i = 1; i < 16; i = i + 1)
+   #5 a_ti = i;
+#5 $finish;
+end
+
+//capture
+initial begin
+$monitor("Time: %0t | Inputs: %b | Majourity: %b | Tie: %b", $time, a_ti, y_to, t_to);
+$dumpfile("mVoting.vcd");
+$dumpvars(0, tb_mVoting);
+end
+
+endmodule
