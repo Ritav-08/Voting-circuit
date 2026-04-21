6bac7a2993cbd851edc7b4b6be87a3e028175207
Ritav <ritav08092004@gmail.com>
2026-04-21 18:28:32 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.04.2026 09:25:04
-// Design Name: 
-// Module Name: mVoting
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mVoting(
    input [3:0]a_i, 
