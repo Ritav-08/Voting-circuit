e6c4a90cd4ac82c2fe4d162ee450210aaedf53d8
Ritav <ritav08092004@gmail.com>
2026-04-21 18:28:53 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.04.2026 09:25:34
-// Design Name: 
-// Module Name: tb_mVoting
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_mVoting();
 reg [3:0]a_ti; 
